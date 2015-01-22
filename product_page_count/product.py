914c1fce9ce448959f93315ad5200fafc62097ae
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-01-22 13:13:30 +0300

    [#63954] Added docstrings and fixed pep8 errors in product.py

--- product_page_count/product.py
+++ product_page_count/product.py
@@ -19,6 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # #############################################################################
+"""This module subclass the product.template."""
 
 import logging
 from openerp.osv import fields, orm
@@ -29,30 +30,58 @@ _logger = logging.getLogger(__name__)
 
 
 class product_product(orm.Model):
+
     """
-    Add page_count field.
+    Add page_count and color_page_count fields.
+
+    The field are used to save the number of normal page
+    in a book and the number of colored page in a book.
     """
+
     _inherit = 'product.template'
 
     _columns = {
         'page_count': fields.integer('Total number of pages', default=0),
-        'color_page_count': fields.integer('Number of pages in color', default=0),
+        'color_page_count': fields.integer('Number of pages in color',
+                                           default=0),
     }
 
     @api.constrains('page_count')
     def _page_count_positive(self):
+        """
+        page_count constraint.
+
+        This function constrains the page_count field to a value
+        higher than 0. Value below 1 are invalid.
+        """
         for record in self:
             if record.page_count < 1:
-                raise ValidationError("The number of page has to be more than zero (0).")
+                raise ValidationError("The number of page has "
+                                      "to be more than zero (0).")
 
     @api.constrains('color_page_count')
     def _color_page_count_positive(self):
+        """
+        color_page_count constraint.
+
+        This function constrains the color_page_count field to a
+        positive value.
+        """
         for record in self:
             if record.color_page_count < 0:
-                raise ValidationError("The number of color page has be equal to or above zero (0).")
+                raise ValidationError("The number of color page has "
+                                      "be equal to or above zero (0).")
 
     @staticmethod
     def _compute_pair_page_count(vals):
+        """
+        Recompute the page_count value.
+
+        It recompute the page_count value to a multiple of two.
+        Pages usually two sides which is why the quantity of page
+        can't be a odd number. This function make sure we don't save
+        an odd number of pages in the database.
+        """
         page_count = vals.get('page_count')
         if page_count and page_count % 2:
             vals['page_count'] = page_count + 1
@@ -61,6 +90,13 @@ class product_product(orm.Model):
 
     @api.model
     def create(self, vals, context=None):
+        """
+        Override the model create method.
+
+        The create method call the _compute_pair_page_count
+        method before the value is saved in the database. It
+        ensure that the number of page is a multiple of two.
+        """
         return super(product_product, self).create(
             self._compute_pair_page_count(vals),
             context=context
@@ -68,6 +104,13 @@ class product_product(orm.Model):
 
     @api.multi
     def write(self, vals, context=None):
+        """
+        Override the model write method.
+
+        The write method call the _compute_pair_page_count
+        method before the value is saved in the database. It
+        ensure that the number of page is a multiple of two.
+        """
         return super(product_product, self).write(
             self._compute_pair_page_count(vals),
             context=context
